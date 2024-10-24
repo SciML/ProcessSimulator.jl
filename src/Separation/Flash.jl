01475a7938a63a701ad7db9e16cc613066f7dd2d
Vinicius Santana <vinicius.viena1@gmail.com>
2024-10-24 09:32:48 +0200

    First work on flash drum - not converging now

--- src/Separation/Flash.jl
+++ src/Separation/Flash.jl
@@ -1,8 +1,9 @@
 @component function ThreePortDrum(; substances,
     non_volatiles::Vector{Bool} = falses(length(substances)),
     non_condensables::Vector{Bool} = falses(length(substances)),
-    Ac,
-    Nc = length(substances), 
+    Vtot_::Real,
+    Ac::Real,
+    Nc::Int = length(substances), 
     model,
     name,
    )
@@ -13,7 +14,9 @@
 g = 9.81 # m/s²
 
 
-pars = []
+pars = @parameters begin
+    Vtot = Vtot_, [description = "Total volume of the drum (m³)"]
+end
 
 
 Ports = @named begin
@@ -25,21 +28,21 @@ end
 
 vars = @variables begin
     (Nᵢᴸ(t))[1:Nc], [description = "Molar holdup of each component in liquid phase (mol)"]
-    (Nᵢᵍ(t))[1:Nc], [description = "Molar holdup of each component in gas phase (mol)"]
-    (xᵢ(t))[1:Nc], [description = "Molar fraction in liquid phase (-)"]
-    (yᵢ(t))[1:Nc], [description = "Molar fraction in gas phase (-)"]
+    (Nᵢᵍ(t))[1:Nc], [description = "Molar holdup of each component in gas phase (mol)", guess = [0.0, 0.0, 10.0]]
+    (xᵢ(t))[1:Nc], [description = "Molar fraction in liquid phase (-)", guess = [0.0, 0.0, 1.0]]
+    (yᵢ(t))[1:Nc], [description = "Molar fraction in gas phase (-)", guess = [0.0, 0.0, 1.0]]
     (Nᵢ(t))[1:Nc], [description = "Molar holdup of each component (mol)"]
     (ϕᵢᴸ(t))[1:Nc], [description = "Fugacity coefficient in liquid phase"]
     (ϕᵢᵍ(t))[1:Nc], [description = "Fugacity coefficient in liquid phase"]
-    Vᴸ(t), [description = "Volume of the liquid phase (m³)"]
-    Vᵍ(t), [description = "Volume of the gas phase (m³)"]
-    ρᴸ(t), [description = "Molar Density of gas phase (mol/m³)"]
+    Vᴸ(t), [description = "Volume of the liquid phase (m³)", guess = 0.5*Vtot_, irreducible = true]
+    Vᵍ(t), [description = "Volume of the gas phase (m³)", guess = 0.5*Vtot_, irreducible = true]
+    ρᴸ(t), [description = "Molar Density of gas phase (mol/m³)", guess = molar_density(model, 1.5*101325.0, 377.0, [0.0, 0.0, 0.001], phase = :liquid)]
     ρᵍ(t), [description = "Molar Density of liquid phase (mol/m³)"]
-    Nᴸ(t), [description = "Total molar holdup in the liquid phase (mol)"] 
-    Nᵍ(t), [description = "Total molar holdup in the gas phase (mol)"] 
+    Nᴸ(t), [description = "Total molar holdup in the liquid phase (mol)", guess = 0.0001] 
+    Nᵍ(t), [description = "Total molar holdup in the gas phase (mol)", guess = 10.0] 
 
     T(t), [description = "Drum temperature (K)"]  
-    P(t), [description = "Drum pressue (Pa)"]
+    P(t), [description = "Drum pressue (Pa)", guess = 1.2*101325.0]
     U(t), [description = "Total internal energy holdup in the tank (L + G) (J)"]
     V(t), [description = "Total volume in the tank (L + G) (m³)"]
 
@@ -47,7 +50,6 @@ vars = @variables begin
     hᵍ_outflow(t), [description = "Outflow Enthalpy of liquid phase (J/mol)"]
     Fᴸ_outflow(t), [description = "Outlet molar flow rate of liquid phase (mol/s)"] 
     Fᵍ_outflow(t), [description = "Outlet molar flow rate of gas phase (mol/s)"] 
-    height(t), [description = "Liquid level in vessel measured from bottom of the tank (m)"]
     Q̇(t), [description = "Heat transfer rate (J/s)"]
 
 
@@ -59,24 +61,25 @@ end
     
 #Conservation equations
 
-liquid_gas_holdups = [Nᴸ ~ sum(collect(Nᵢᴸ)), Nᵍ ~ sum(collect(Nᵢᵍ))]
+#fractions_cons = [0.0 ~ sum(collect(xᵢ)) - sum(collect(yᵢ))]
 
-xy_fractions = [scalarize(xᵢ.*Nᴸ .~ Nᵢᴸ)...; scalarize(yᵢ.*Nᵍ .~ Nᵢᵍ)]
+liquid_gas_holdups = [sum(collect(Nᵢ)) ~ Nᵍ + Nᴸ]
+
+xy_fractions = [scalarize(xᵢ.*Nᴸ .~ Nᵢᴸ)...; scalarize(yᵢ.*Nᵍ .~ Nᵢᵍ)...]
 
 component_holdups = [Nᵢ[i] ~ Nᵢᴸ[i] + Nᵢᵍ[i] for i in 1:Nc]
-
 
 component_balance = [D(Nᵢ[i]) ~ In.F*In.z₁[i] - Fᴸ_outflow*xᵢ[i] - Fᵍ_outflow*yᵢ[i] for i in 1:Nc]
 
 energy_balance = [D(U) ~ In.F*In.H - Fᴸ_outflow*hᴸ_outflow - Fᵍ_outflow*hᵍ_outflow + Q̇]
 
-heat_source_equation = [Q̇ ~ 0.0]
+heat_source_equation = [Q̇ ~ 9000.0]
 
 #Thermodynamic constraints
 
 internal_energy = [U + P*V ~ Nᴸ*hᴸ_outflow + Nᵍ*hᵍ_outflow] 
 
-total_volume = [V ~ Vᴸ + Vᵍ] #That should not be fixed a priori. One may want to have a perfect pressure control vessel. 
+total_volume = [V ~ Vᴸ + Vᵍ, V ~ Vtot] 
 
 liquid_gas_enthalpy = [hᴸ_outflow ~ enthalpy(model, P, T, xᵢ, phase = "liquid")
  hᵍ_outflow ~ enthalpy(model, P, T, yᵢ, phase = "vapor")]
@@ -88,20 +91,20 @@ liquid_volume = [ρᴸ*Vᴸ ~ Nᴸ]
 
 gas_volume = [ρᵍ*Vᵍ ~ Nᵍ]
 
-fugacities_g = [ϕᵢᵍ[i] ~ fugacity_coefficient(model, P, T, Nᵢᵍ, phase = "vapor")[i] for i in 1:Nc if ~non_condensables[i] & ~non_volatiles[i]]
+fugacities_g = [ϕᵢᵍ[i] ~ fugacity_coefficient(model, P, T, yᵢ, phase = "vapor")[i] for i in 1:Nc]
 
-fugacities_l = [ϕᵢᴸ[i] ~ fugacity_coefficient(model, P, T, Nᵢᴸ, phase = "liquid")[i] for i in 1:Nc if ~non_condensables[i] & ~non_volatiles[i]]
+fugacities_l = [ϕᵢᴸ[i] ~ fugacity_coefficient(model, P, T, xᵢ, phase = "liquid")[i] for i in 1:Nc]
 
 non_smoothness = [_0_Nᵍ ~ Nᵍ/(Nᵍ + Nᴸ)
  _0_Nᴸ ~ Nᵍ/(Nᵍ + Nᴸ) - 1.0
- 0.0 ~ _0_Nᵍ + _0_Nᴸ + sum(collect(xᵢ)) - sum(collect(yᵢ)) - min(_0_Nᵍ, _0_Nᴸ, sum(collect(xᵢ)) - sum(collect(yᵢ))) - max(_0_Nᵍ, _0_Nᴸ, sum(collect(xᵢ)) - sum(collect(yᵢ))) 
+ 0.0 ~ _0_Nᵍ + _0_Nᴸ + (sum(collect(xᵢ)) - sum(collect(yᵢ))) - min(_0_Nᵍ, _0_Nᴸ, sum(collect(xᵢ)) - sum(collect(yᵢ))) - max(_0_Nᵍ, _0_Nᴸ, sum(collect(xᵢ)) - sum(collect(yᵢ))) 
  ] #Non-smooth formulation 
 
- equilibrium = [ϕᵢᵍ[i].*yᵢ[i] .~ ϕᵢᴸ[i].*xᵢ[i] for i in 1:Nc if ~non_condensables[i] & ~non_volatiles[i]]
+ equilibrium = [ϕᵢᵍ[i].*yᵢ[i] .~ ϕᵢᴸ[i].*xᵢ[i] for i in 1:Nc]
 
  # Out connectors
 
- out_conn_L = [Outᴸ.P ~ P + ρᴸ*g*Ac/Vᴸ
+ out_conn_L = [Outᴸ.P ~ P + g*Vᴸ/Ac
  Outᴸ.T ~ T
  Outᴸ.F ~ Fᴸ_outflow
  Outᴸ.H ~ hᴸ_outflow
--- test/Flash_test/FlashDrum.jl
+++ test/Flash_test/FlashDrum.jl
@@ -2,10 +2,11 @@
 
 using Revise
 using ProcessSimulator
-using ModelingToolkit, OrdinaryDiffEq, Clapeyron
+using ModelingToolkit, DifferentialEquations, Clapeyron
 #import ModelingToolkit: get_unknowns, get_observed, get_defaults, get_eqs, scalarize
 using ModelingToolkit: t_nounits as t, D_nounits as D
 using ProcessSimulator: matcon
+using NonlinearSolve
 
 substances = ["benzene", "toluene", "nitrogen"]
 
@@ -13,21 +14,85 @@ idealmodel = ReidIdeal(substances)
 
 PR_model = PR(substances, idealmodel = idealmodel, translation = RackettTranslation)
 
-@named Flash = ThreePortDrum(; substances = substances, 
-    non_condensables = [false, false, true],
+molar_density(PR_model, 1.5*101325.0, 400.0, 1.0*[0.5, 0.5, 0.5], phase = :vapor)
+
+x, n, G = Clapeyron.tp_flash(PR_model, 1.5*101325.0, 50.0, [0.0, 1.0, 1.0], DETPFlash())
+
+bubble_temperature(PR_model, 1.5*101325.0, [0.0, 1.0, 1.0])
+
+@named Drum_101 = ThreePortDrum(; substances = substances, 
+    Vtot_ = 0.2,
+    non_condensables = [false, false, false],
     non_volatiles = [false, false, false],
     Ac = 0.1,
     model = PR_model)
 
+
+for eq in equations(Drum_101)
+    println(eq)
+end
+
 @named source = MaterialSource(;substances_user =  substances,
 model = PR_model,
 P_user = 1.5*101325.0,
-T_user = 350.0,
-Fₜ_user = 2.0,
+T_user = 377.0, #Kelvin
+Fₜ_user = 2.0, #mol/s
 zₜ_user = [0.5, 0.5, 0.0], 
-guesses = Dict((:zᵂⱼᵢ) => ones(3, 4)*0.25))
+guesses = Dict((:zᵂⱼᵢ) => ones(3, 3)*0.33))
 
 
-for eq in equations(Flash)
+for eq in equations(source)
     println(eq)
 end
+
+
+@named Liquid_Valve = LinearValve(;
+    Nc = 3,
+    CV = 0.141,
+    model = PR_model)
+
+@named Vapor_Valve = LinearValve(;
+    Nc = 3, 
+    CV = 0.01,
+    model = PR_model)
+
+
+
+cons = [connect(source.Out, Drum_101.In),
+
+connect(Drum_101.Outᴸ, Liquid_Valve.In),
+
+connect(Drum_101.Outᵍ, Vapor_Valve.In)]
+
+check_and_block = [(Drum_101.Vᴸ <= 0.01*0.2) => [Liquid_Valve.θ ~ 0.0]
+                     (Drum_101.Vᵍ <= 0.02*0.2) => [Vapor_Valve.θ ~ 0.0]
+                     (Drum_101.P - Liquid_Valve.P <= 0.0)  => [Liquid_Valve.θ ~ 0.0]
+                     (Drum_101.P - Vapor_Valve.P <= 0.0) => [Vapor_Valve.θ ~ 0.0]] 
+
+
+flowsheet = ODESystem(cons, t; name = :myflowsheet, systems = [Drum_101, source, Liquid_Valve, Vapor_Valve], discrete_events = check_and_block)   
+
+sistema = structural_simplify(flowsheet)
+
+u0 = [sistema.Drum_101.Nᵢ[1] => 0.0, sistema.Drum_101.Nᵢ[2] => 0.0,
+ sistema.Drum_101.Nᵢ[3] => 10.0,
+ sistema.Drum_101.T => 377.0, sistema.Liquid_Valve.θ => 0.5, sistema.Vapor_Valve.θ => 0.5]
+
+unknowns(sistema)
+alg_equations(sistema)
+
+prob = ODEProblem(sistema, u0, (0.0, 0.01))
+
+dt = 1e-7
+sol = solve(prob, ImplicitEuler(nlsolve = NLNewton(check_div=false, always_new=true, relax=4/10, max_iter=100)); dt, adaptive=false)
+sol = solve(prob, FBDF())
+
+sol[sistema.Drum_101.yᵢ]
+a = sum.(sol[sistema.Drum_101.xᵢ]) - sum.(sol[sistema.Drum_101.yᵢ])
+b = sol[sistema.Drum_101._0_Nᴸ]
+c = sol[sistema.Drum_101._0_Nᵍ]
+
+my_med(a,b,c) = a + b + c - min(a,b,c) - max(a,b,c)
+
+my_med(a, b, c)
+
